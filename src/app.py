d5cf2bfd317cafa001d04fe5c74e62cdb6756ab4
Joseph Angel <josephmangel01@gmail.com>
2024-02-15 16:03:22 -0700

    reverted app.py to orig

--- src/app.py
+++ src/app.py
@@ -7,29 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return '''
-     <html>
-     <head>
-         <style>
-             body {
-                 display: flex;
-                 align-items: center;
-                 justify-content: center;
-                 height: 100vh;
-                 margin: 0;
-             }
-
-             form {
-                 text-align: center;
-             }
-         </style>
-     </head>
-     <body>
-         <form action="/echo_user_input" method="POST">
-             <input name="user_input">
-             <input type="submit" value="Submit!">
-         </form>
-     </body>
-     </html>
+     <form action="/echo_user_input" method="POST">
+         <input name="user_input">
+         <input type="submit" value="Submit!">
+     </form>
      '''
 
 @app.route("/echo_user_input", methods=["POST"])
