61f278f133fdd30fa88750a824a5c57319f46e25
Joseph Angel <josephmangel01@gmail.com>
2024-02-21 17:49:55 -0700

    adjusted comments in app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Use the DATABASE_URL environment variable provided by Heroku
+# Use the database URL provided by Heroku
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e'
 
 db = SQLAlchemy(app)
@@ -15,7 +15,7 @@ def main():
     query = "SELECT * FROM player_career_stats;"
     data_frame = pd.read_sql_query(query, db.engine)
 
-    # Render the data in an HTML table
+    # Show the data in an HTML table
     table_html = data_frame.to_html(classes='table table-striped', index=False)
 
     return f'''
