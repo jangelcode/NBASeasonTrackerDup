192f1e29a0bfb8d69e3ef7b7c088619f6019489b
Joseph Angel <josephmangel01@gmail.com>
2024-02-22 14:56:35 -0700

    new virtual environment

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\AppOfBigData\my_flask_app\venv
+set VIRTUAL_ENV=C:\AppOfBigData\nba_web_app\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
