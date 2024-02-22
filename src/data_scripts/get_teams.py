6e34de8b482ede01334a02c9a078222cce61591b
Joseph Angel <josephmangel01@gmail.com>
2024-02-22 15:04:49 -0700

    fix get_teams.py

--- src/data_scripts/get_teams.py
+++ src/data_scripts/get_teams.py
@@ -1,10 +1,9 @@
 from nba_api.stats.static import teams
 
-# Retrieve all teams
+# Retrieve list of all teams information
 all_teams = teams.get_teams()
 
-# Extract team IDs from the response
+# Extract team IDs from each team
 team_ids = [team['id'] for team in all_teams]
 
-# Print or use the list of team IDs
 print(team_ids)
