7d949277a96aea4031784b984a7dbcb46ef6ae54
Joseph Angel <josephmangel01@gmail.com>
2024-02-21 17:44:02 -0700

    change app to correct postgres URL

--- src/app.py
+++ src/app.py
@@ -1,14 +1,11 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects import postgresql  # Import the PostgreSQL dialect
 import pandas as pd
 
 app = Flask(__name__)
 
 # Use the DATABASE_URL environment variable provided by Heroku
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Recommended to suppress a warning
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e'
 
 db = SQLAlchemy(app)
 
@@ -22,7 +19,7 @@ def main():
     table_html = data_frame.to_html(classes='table table-striped', index=False)
 
     return f'''
-     <h1>NBA Player Career Stats</h1>
+     <h1>NBA Player Career Stats for Nikola Jokic</h1>
      {table_html}
      '''
 
