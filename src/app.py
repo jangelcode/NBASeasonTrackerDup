37f6b1d270c4772f73aa1799119d8ba763a50e73
Joseph Angel <josephmangel01@gmail.com>
2024-02-23 13:54:55 -0700

    column labels and comments

--- src/app.py
+++ src/app.py
@@ -1,19 +1,17 @@
 from flask import Flask
 import pandas as pd
 from sqlalchemy import create_engine
-from data_scripts.defense_data import fill_database
 
 app = Flask(__name__)
 
-# Your PostgreSQL database URL
+#postgreSQL database URL
 database_url = "postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e"
 
-# Connect to the PostgreSQL database
 engine = create_engine(database_url)
 
 @app.route("/")
 def main():
-    query = "SELECT * FROM your_table_name;"
+    query = "SELECT * FROM teams;"
     df = pd.read_sql(query, con=engine)
 
     table_html = df.to_html(classes='table table-striped', index=False)
@@ -21,7 +19,7 @@ def main():
     return f'''
         <html>
             <head>
-                <header>Team Stats</header>
+                <h1>NBA Team Stats</h1>
             </head>
             <body>
                 {table_html}
--- src/app_test.py
+++ src/app_test.py
@@ -4,24 +4,18 @@ from app import app
 class TestFlaskApp(unittest.TestCase):
 
     def setUp(self):
-        # Create a test client
+        #create client
         self.app = app.test_client()
-        # Set Flask to testing mode
+        #set testing mode
         app.config['TESTING'] = True
 
-    def tearDown(self):
-        pass  # Clean up, if needed
-
     def test_main_route(self):
-        # Send a GET request to the / endpoint
         response = self.app.get('/')
 
-        # Check if the status code is 200 (OK)
+        #check status code is 200
         self.assertEqual(response.status_code, 200)
 
-        # You can also check for specific content in the response, for example, checking if the HTML contains a specific string
-        self.assertIn(b'<h1>NBA Player Career Stats for Nikola Jokic</h1>', response.data)
-        # You might want to check for other specific content or patterns in the HTML response as needed.
+        self.assertIn(b'<h1>NBA Team Stats</h1>', response.data)
 
 if __name__ == '__main__':
     unittest.main()
