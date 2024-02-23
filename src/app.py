8b14998f0e49229d6c38dca05e75b0bf65e270c7
Joseph Angel <josephmangel01@gmail.com>
2024-02-23 00:05:56 -0700

    new app with api request

--- src/app.py
+++ src/app.py
@@ -1,27 +1,39 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+import requests
 import pandas as pd
 
 app = Flask(__name__)
 
-# Use the Heroku database URL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e'
-
-db = SQLAlchemy(app)
-
 @app.route("/")
 def main():
-    # Get data from the database
-    query = "SELECT * FROM player_career_stats;"
-    data_frame = pd.read_sql_query(query, db.engine)
+    # API request to get data
+    url = "https://api-basketball.p.rapidapi.com/statistics"
+    querystring = {"season": "2023-2024", "league": "12", "team": "133"}
+    headers = {
+        "X-RapidAPI-Key": "02d9b0d232msh33e360bbdbbf28cp14fc09jsn2aaba1786409",
+        "X-RapidAPI-Host": "api-basketball.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
 
-    # Create HTML table for data
-    table_html = data_frame.to_html(classes='table table-striped', index=False)
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
 
-    return f'''
-     <h1>NBA Player Career Stats for Nikola Jokic</h1>
-     {table_html}
-     '''
+        # Extract relevant information from the API response and create a Pandas DataFrame
+        # Adjust the code based on the actual structure of the API response
+        # For example, if the data is nested under a specific key, use data['key']
+        df = pd.DataFrame(data['response'])
+
+        # Create HTML table for data
+        table_html = df.to_html(classes='table table-striped', index=False)
+
+        return render_template('index.html', table_html=table_html)
+
+    else:
+        # Handle the case where the API request was not successful
+        return f"Error: {response.status_code} - {response.text}"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
