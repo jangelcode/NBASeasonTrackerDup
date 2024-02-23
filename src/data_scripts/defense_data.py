b1c26dffd38b6b0454ae761c23ce15b9585da6ae
Joseph Angel <josephmangel01@gmail.com>
2024-02-22 20:06:29 -0700

    change player defense_data.py

--- src/data_scripts/defense_data.py
+++ src/data_scripts/defense_data.py
@@ -19,10 +19,10 @@ def get_nba_player_stats(player_id):
 engine = create_engine('postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e')
 
 # Get NBA player career stats for Nikola Jokic
-player_id = '203999'
+player_id = '203998'
 nba_stats_df = get_nba_player_stats(player_id)
 
-# Store data in SQLite database
+# Store data in postgres database
 nba_stats_df.to_sql('player_career_stats', engine, if_exists='replace', index=False)
 
 # Dispose of the database engine
