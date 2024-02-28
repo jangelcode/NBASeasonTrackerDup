cbb23d19d42690dafe294258b6c08e7b58ef7a1a
Joseph Angel <josephmangel01@gmail.com>
2024-02-28 13:02:36 -0700

    comments for app.py

--- src/app.py
+++ src/app.py
@@ -8,23 +8,23 @@ app = Flask(__name__)
 database_url = "postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e"
 engine = create_engine(database_url)
 
-
+#render home page
 @app.route("/")
 def home():
-    return render_template("index.html")  # This will be your home page with tabs
+    return render_template("index.html")
 
-
+#rankings page
 @app.route("/rankings")
 def rankings():
     query = 'SELECT * FROM teams ORDER BY "Pct" DESC;'
     df = pd.read_sql(query, con=engine)
     table_html = df.to_html(classes='table table-striped', index=False, justify='left')
-    return render_template("rankings.html", table_html=table_html)  # Pass the rankings data to the template
+    return render_template("rankings.html", table_html=table_html)
 
-
+#prediction page
 @app.route("/make-a-prediction")
 def make_a_prediction():
-    return render_template("prediction.html")  # Assuming you have a prediction.html template
+    return render_template("prediction.html")
 
 
 if __name__ == "__main__":
