bd058c16294b025551387c58bcfe5905e03be370
Joseph Angel <josephmangel01@gmail.com>
2024-02-28 15:51:55 -0700

    fill_database > get_team_data

--- src/data_scripts/team_data.py
+++ src/data_scripts/team_data.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 from sqlalchemy import create_engine
 
-def fill_database():
+def get_team_data():
     database_url = "postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e"
     url = "https://api-basketball.p.rapidapi.com/standings"
 
@@ -55,4 +55,4 @@ def fill_database():
     all_data.to_sql('teams', con=engine, index=False, if_exists='replace')
     engine.dispose()
 
-fill_database()
+get_team_data()
