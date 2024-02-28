50d451fffa19e4152ec3b86a677999922cd5d5e6
Joseph Angel <josephmangel01@gmail.com>
2024-02-28 11:40:57 -0700

    adjust formatting

--- src/data_scripts/team_data.py
+++ src/data_scripts/team_data.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 
 def fill_database():
     database_url = "postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e"
-
     url = "https://api-basketball.p.rapidapi.com/standings"
 
     headers = {
