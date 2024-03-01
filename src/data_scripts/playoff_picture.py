cfe33d898246a86084aea093d8a679f6f09b58b9
Joseph Angel <josephmangel01@gmail.com>
2024-03-01 13:57:17 -0700

    adjust playoff_picture

--- src/data_scripts/playoff_picture.py
+++ src/data_scripts/playoff_picture.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 def get_playoff_teams():
     database_url = "postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e"
     engine = create_engine(database_url)
-    query = f'SELECT "Team" FROM teams;'
+    query = f'SELECT "Team", "Conf. Standings", "Conference" FROM teams;'
 
     df = pd.read_sql(query, con=engine)
     engine.dispose()
@@ -16,9 +16,11 @@ def get_playoff_teams():
 def get_conferences(df):
     playoff_picture_east = []
     playoff_picture_west = []
+
     for index, row in df.iterrows():
         if row["Conf. Standings"] <= 10 and row["Conference"] == 'Eastern Conference':
             playoff_picture_east.append(row["Team"])
         elif row["Conf. Standings"] <= 10:
             playoff_picture_west.append(row["Team"])
-    
+
+    return playoff_picture_east, playoff_picture_west
