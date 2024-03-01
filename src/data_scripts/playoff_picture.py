0a5b3f774709ca43e92d1f81a007024015b45387
Joseph Angel <josephmangel01@gmail.com>
2024-03-01 13:41:20 -0700

    divide playoff_picture

--- src/data_scripts/playoff_picture.py
+++ src/data_scripts/playoff_picture.py
@@ -1,13 +1,19 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+
 def get_playoff_teams():
     database_url = "postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e"
     engine = create_engine(database_url)
     query = f'SELECT "Team" FROM teams;'
 
     df = pd.read_sql(query, con=engine)
+    engine.dispose()
 
+    return get_conferences(df)
+
+
+def get_conferences(df):
     playoff_picture_east = []
     playoff_picture_west = []
     for index, row in df.iterrows():
@@ -16,7 +22,3 @@ def get_playoff_teams():
         elif row["Conf. Standings"] <= 10:
             playoff_picture_west.append(row["Team"])
     
-    engine.dispose()
-
-    return playoff_picture_east, playoff_picture_west
-
