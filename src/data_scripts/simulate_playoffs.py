04e36683ea38dc7b86b3e1aecc6c45909b63a8c0
Joseph Angel <josephmangel01@gmail.com>
2024-03-04 13:19:27 -0700

    clean up simulate_playoffs and team_data

--- src/data_scripts/simulate_playoffs.py
+++ src/data_scripts/simulate_playoffs.py
@@ -1,11 +1,18 @@
-from flask import Flask, request
+from flask import request
 from sqlalchemy import create_engine
 import pandas as pd
-from data_scripts.team_data import get_team_data
 
 
+def scale_data(series, invert=False):
+    # Scale data to a 0-10 range. Invert if lower values are considered better.
+    if invert:
+        scaled_series = (1 - (series - series.min()) / (series.max() - series.min())) * 10
+    else:
+        scaled_series = ((series - series.min()) / (series.max() - series.min())) * 10
+    return scaled_series
 
-def predict_winner():
+
+def get_playoff_weights():
     # Extract slider values from the form
     point_diff_weight = float(request.form['pointDifferential']) / 10
     ppg_weight = float(request.form['pointsPerGame']) / 10
@@ -13,33 +20,43 @@ def predict_winner():
     oppg_pct_weight = float(request.form['OPPG']) / 10
     standings_pct_weight = float(request.form['confStandings']) / 10
 
-    
+    return point_diff_weight, ppg_weight, win_pct_weight, oppg_pct_weight, standings_pct_weight
+
+
+def get_playoff_data():
     # Load team data
     database_url = "postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e"
     engine = create_engine(database_url)
-
-    query = f'SELECT * FROM teams;'
+    query = f'SELECT "Team", "Point Differential", "PPG", "Pct", "OPPG", "Conf. Standings" FROM teams;'
     team_data = pd.read_sql(query, con=engine)
-    team_data_point_diff = (team_data['Point Differential'].astype(float) / max(team_data['Point Differential'].astype(float))) * 10
-    team_data_ppg = (team_data['PPG'].astype(float) / max(team_data['PPG'].astype(float))) * 10
-    team_data_pct = (team_data['Pct'].astype(float) / max(team_data['Pct'].astype(float))) * 10
-    team_data_current_standings = (team_data['Conf. Standings'].astype(float) / max(team_data['Conf. Standings'].astype(float))) * 10
-    max_oppg = max(team_data['OPPG'].astype(float))
-    team_data_oppg = (1 - (team_data['OPPG'].astype(float) / max_oppg)) * 10
-    
-    # Calculate weighted score for each team
-    team_data['Score'] = round((team_data_point_diff * point_diff_weight) +
-                          (team_data_ppg * ppg_weight) +
-                          (team_data_pct * win_pct_weight) + 
-                          (team_data_oppg * oppg_pct_weight) +
-                          (team_data_current_standings * standings_pct_weight), 2)
-    # team_data['Score'] = round(team_data['Score'] / max(team_data['Score']) * 10, 2)
-    
-    # Predict winner as the team with the highest score
-    predicted_winner = team_data.loc[team_data['Score'].idxmax(), 'Team']
+    team_data = team_data.apply(lambda x: x.astype(float) if x.name != 'Team' else x)
+    return team_data
 
-    prediction_results = team_data[['Team', 'Score']].sort_values(by='Score', ascending=False).head(10)
-    
-    msg = (f"The predicted NBA Championship winner is: {predicted_winner}")
 
+def calculate_team_scores(team_data, weights):
+    """Calculate weighted scores for each team based on scaled team data and weights."""
+    # Scale team statistics
+    team_data['Point Differential'] = scale_data(team_data['Point Differential'])
+    team_data['PPG'] = scale_data(team_data['PPG'])
+    team_data['Pct'] = scale_data(team_data['Pct'])
+    team_data['OPPG'] = scale_data(team_data['OPPG'], invert=True)  # Invert since lower is better
+    team_data['Conf. Standings'] = scale_data(team_data['Conf. Standings'], invert=True)  # Invert standings
+    
+    # Apply weights
+    point_diff_weight, ppg_weight, win_pct_weight, oppg_pct_weight, standings_pct_weight = weights
+    team_data['Score'] = round((team_data['Point Differential'] * point_diff_weight) +
+                               (team_data['PPG'] * ppg_weight) +
+                               (team_data['Pct'] * win_pct_weight) + 
+                               (team_data['OPPG'] * oppg_pct_weight) +
+                               (team_data['Conf. Standings'] * standings_pct_weight), 2)
+    return team_data
+
+
+def predict_winner():
+    team_data = get_playoff_data()
+    weights = get_playoff_weights()
+    scored_teams = calculate_team_scores(team_data, weights)
+    predicted_winner = scored_teams.loc[scored_teams['Score'].idxmax(), 'Team']
+    prediction_results = scored_teams[['Team', 'Score']].sort_values(by='Score', ascending=False).head(10)
+    msg = f"The predicted NBA Championship winner is: {predicted_winner}"
     return msg, prediction_results
--- src/data_scripts/team_data.py
+++ src/data_scripts/team_data.py
@@ -2,58 +2,69 @@ import requests
 import pandas as pd
 from sqlalchemy import create_engine
 
-def get_team_data():
-    database_url = "postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e"
+
+def fetch_team_data(team_id):
     url = "https://api-basketball.p.rapidapi.com/standings"
     headers = {
         "X-RapidAPI-Key": "02d9b0d232msh33e360bbdbbf28cp14fc09jsn2aaba1786409",
         "X-RapidAPI-Host": "api-basketball.p.rapidapi.com"
     }
+    querystring = {"season": "2023-2024", "league": "12", "team": str(team_id)}
+    response = requests.get(url, headers=headers, params=querystring)
+    data = response.json()
+    return data
 
-    #init data frame and apis team ids
+
+def extract_team_info(data):
+    team_info = data['response'][0][0]['team']
+    conference_info = data['response'][0][0]['group']
+    games_info = data['response'][0][0]['games']
+    points_info = data['response'][0][0]['points']
+    
+    row_data = {
+        'Team': team_info['name'],
+        'Conf. Standings': data['response'][0][0]['position'],
+        'Conference': conference_info['name'],
+        'Wins': games_info['win']['total'],
+        'Loses': games_info['lose']['total'],
+        'Played': games_info['played'],
+        'Pct': games_info['win']['percentage'],
+        'Points Scored': points_info['for'],
+        'Points Allowed': points_info['against'],
+        'Point Differential': points_info['for'] - points_info['against'],
+        'PPG': round(points_info['for'] / games_info['played'], 1),
+        'OPPG': round(points_info['against'] / games_info['played'], 1),
+    }
+    
+    return row_data
+
+
+def aggregate_team_data():
     team_ids = range(132, 162)
     all_data = pd.DataFrame()
 
     for team_id in team_ids:
-        querystring = {"season": "2023-2024", "league": "12", "team": str(team_id)}
-        response = requests.get(url, headers=headers, params=querystring)
-        data = response.json()
-
-
-        #extract data from response categories
-        team_info = data['response'][0][0]['team']
-        conference_info = data['response'][0][0]['group']
-        games_info = data['response'][0][0]['games']
-        points_info = data['response'][0][0]['points']
+        data = fetch_team_data(team_id)
+        row_data = extract_team_info(data)
         
-        #create dictionary
-        row_data = {
-            #pull from data
-            'Team': team_info['name'],
-            'Conf. Standings': data['response'][0][0]['position'],
-            'Conference': conference_info['name'],
-            'Wins': games_info['win']['total'],
-            'Loses': games_info['lose']['total'],
-            'Played': games_info['played'],
-            'Pct': games_info['win']['percentage'],
-            'Points Scored': points_info['for'],
-            'Points Allowed': points_info['against'],
-
-            #calculate other stats using provided data
-            'Point Differential': points_info['for'] - points_info['against'],
-            'PPG': round(points_info['for'] / games_info['played'], 1),
-            'OPPG': round(points_info['against'] / games_info['played'], 1),
-        }
-        
-        #add to dataframe
         if not all_data.empty:
-            all_data.loc[team_id-132] = row_data
+            all_data = pd.concat([all_data, pd.DataFrame([row_data])], ignore_index=True)
         else:
             all_data = pd.DataFrame([row_data])
+    
+    return all_data
 
+
+def store_team_data(data_frame):
+    database_url = "postgresql://pcvqvgmijraryx:26c43ba15b78faf8bbf3b162d8f743b9ec3d741cabd07856f210bd7b0fc82dd8@ec2-34-230-120-83.compute-1.amazonaws.com:5432/d2m4f9jdj48v0e"
     engine = create_engine(database_url)
-    all_data.to_sql('teams', con=engine, index=False, if_exists='replace')
+    data_frame.to_sql('teams', con=engine, index=False, if_exists='replace')
     engine.dispose()
+
+
+def get_team_data():
+    all_data = aggregate_team_data()
+    store_team_data(all_data)
     return all_data
 
 get_team_data()
