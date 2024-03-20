5cf9f117b14bd269bfacae3e1893a8be4bd5ee98
jangelcode <83419734+jangelcode@users.noreply.github.com>
2024-03-20 16:52:17 -0700

    Update playoff_status_test.py

--- tests/data_scripts/playoff_status_test.py
+++ tests/data_scripts/playoff_status_test.py
@@ -12,7 +12,7 @@ sys.path.append(src_path)
 from data_scripts.playoff_status import get_playoff_status
 
 #connect to database
-database_url = "postgresql://fizcsntuttpigc:01594c15291a6eeaba874716fa449c578d303f263b94082efcce5c29ecbaf579@ec2-18-204-162-101.compute-1.amazonaws.com:5432/dbmcmt26draeuq"
+database_url = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 engine = create_engine(database_url)
 
 #check that a #1 seed makes the playoffs
