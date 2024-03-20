c6dcc34f9df7db39137ee9403dec481d8ac0d3a1
jangelcode <83419734+jangelcode@users.noreply.github.com>
2024-03-20 16:48:44 -0700

    Update database.py

--- src/data_scripts/database.py
+++ src/data_scripts/database.py
@@ -1,2 +1,2 @@
 def get_database_URI():
-    return "postgresql://fizcsntuttpigc:01594c15291a6eeaba874716fa449c578d303f263b94082efcce5c29ecbaf579@ec2-18-204-162-101.compute-1.amazonaws.com:5432/dbmcmt26draeuq"
+    return "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
