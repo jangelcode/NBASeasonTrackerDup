523b25bfcbfe3011ce536af8c517e510767da335
jangelcode <83419734+jangelcode@users.noreply.github.com>
2024-03-20 16:49:40 -0700

    Update playoff_status.py

--- src/data_scripts/playoff_status.py
+++ src/data_scripts/playoff_status.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 
 #get playoff status for each team (playoff, playin, or miss)
 def get_playoff_status(team_name):
-    database_url = "postgresql://fizcsntuttpigc:01594c15291a6eeaba874716fa449c578d303f263b94082efcce5c29ecbaf579@ec2-18-204-162-101.compute-1.amazonaws.com:5432/dbmcmt26draeuq"
+    database_url = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     engine = create_engine(database_url)
     query = f'SELECT "Team", "Conf. Standings"  FROM teams WHERE "Team"=\'{team_name}\';'
 
